59631c8f474e489902f478ddf6b5bb39ef5556aa
Vladimir Zarypov <krre31@gmail.com>
2023-09-09 16:34:42 +0300

    Add `Axum` web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
     println!("Norm Developer Server started");
+
+    let app = Router::new().route("/", get(|| async { "Norm Developer Server" }));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
