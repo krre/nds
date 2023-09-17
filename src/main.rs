43da2f558bce46cc63b66fd59d02272392c0cfc1
Vladimir Zarypov <krre31@gmail.com>
2023-09-17 14:39:33 +0300

    Add return value to `main` function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use std::error::Error;
+
 use axum::{routing::get, Router};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("Norm Developer Server started");
 
     let app = Router::new().route("/", get(|| async { "Norm Developer Server" }));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
