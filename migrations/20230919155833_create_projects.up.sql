6d0ebd125abf0f0720439e7702b4f061d01865e4
Vladimir Zarypov <krre31@gmail.com>
2023-09-19 19:13:07 +0300

    Add migration to create `projects` table

---
+++ migrations/20230919155833_create_projects.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS projects (
+    id serial PRIMARY KEY,
+    name TEXT NOT NULL,
+    template smallint NOT NULL,
+    created_ts timestamptz NOT NULL DEFAULT now(),
+    updated_ts timestamptz NOT NULL DEFAULT now()
+);
