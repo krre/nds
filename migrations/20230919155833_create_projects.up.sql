0a446666e6ed8c1b68871dbd85df8fbd9703b2dd
Vladimir Zarypov <krre31@gmail.com>
2023-09-27 07:34:51 +0300

    Add `description` field to `projects` table

--- migrations/20230919155833_create_projects.up.sql
+++ migrations/20230919155833_create_projects.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS projects (
     id serial PRIMARY KEY,
     name TEXT NOT NULL,
     template smallint NOT NULL,
+    description TEXT,
     created_ts timestamptz NOT NULL DEFAULT now(),
     updated_ts timestamptz NOT NULL DEFAULT now()
 );
